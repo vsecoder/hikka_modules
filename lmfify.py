9e955dad0ba588d8ea0b686d41bb19ff6e778fb9
Всеволод <kreepmeister@gmail.com>
2023-06-08 14:55:48 +0300

    lmfify

---
+++ lmfify.py
@@ -0,0 +1,65 @@
+"""
+                                _             
+  __   _____  ___  ___ ___   __| | ___ _ __   
+  \ \ / / __|/ _ \/ __/ _ \ / _` |/ _ \ '__|  
+   \ V /\__ \  __/ (_| (_) | (_| |  __/ |     
+    \_/ |___/\___|\___\___/ \__,_|\___|_|     
+
+    Copyleft 2022 t.me/vsecoder                                                            
+    This program is free software; you can redistribute it and/or modify 
+
+"""
+# meta developer: @vsecoder_m
+# meta pic: https://img.icons8.com/bubbles/344/google-logo.png
+
+__version__ = (2, 0, 0)
+
+import logging
+import asyncio
+from .. import loader, utils
+
+logger = logging.getLogger(__name__)
+
+
+@loader.tds
+class LMFIFYMod(loader.Module):
+    """Let me find it for you in Google / Yandex"""
+
+    strings = {
+        "name": "LMFIFY",
+        "cfg_searc_engine": "Searcher, http://yaforyou.ru/?= or https://track24.ru/google/?q=",
+        "answer": "<b><emoji document_id=5276167890624585747>👩‍💻</emoji> Let me find it for you: </b><a href='{}'>👉click</a>",
+        "error": "Need text!",
+    }
+
+    strings_ru = {
+        "cfg_searc_url": "Поисковик, http://yaforyou.ru/?= или https://track24.ru/google/?q=",
+        "answer": "<b><emoji document_id=5276167890624585747>👩‍💻</emoji> Дай-ка я найду: </b><a href='{}'>👉click</a>",
+        "error": "Нужен текст!",
+    }
+
+    def __init__(self):
+        self.config = loader.ModuleConfig(
+            "search_url",
+            "https://track24.ru/google/?q={query}",
+            lambda m: self.strings("cfg_searc_engine", m),
+        )
+        self.name = self.strings["name"]
+
+    async def client_ready(self, client, db):
+        self._client = client
+
+    @loader.unrestricted
+    @loader.ratelimit
+    async def finditcmd(self, message):
+        """
+        {text} - find it in search engine
+        """
+        args = utils.get_args_raw(message)
+        if args:
+            url = self.config["search_url"].format(query=args).replace(" ", "+")
+            await utils.answer(message, self.strings["answer"].format(url))
+        else:
+            await utils.answer(message, self.strings["error"])
+            await asyncio.sleep(5)
+            await message.delete()
