6cf599b834f163546cbcee984e1ad815506a6aa3
Всеволод <60792943+vsecoder@users.noreply.github.com>
2024-04-04 10:32:30 +0300

    Fix lib

--- libs/sub.py
+++ libs/sub.py
@@ -1,14 +1,14 @@
 from telethon.tl.functions.channels import JoinChannelRequest
-
+import asyncio
 
 class CheckSubscribe:
     def __init__(self, client, channel_name="vsecoder_m"):
         super().__init__()
         self.channel_name = channel_name
         self.client = client
-        self.is_sub = self.check()
-        
-    def check(self):
+        self.is_sub = asyncio.run(self.check)
+
+    async def check(self):
         try:
             channel = await self.client.get_entity(f"t.me/{self.channel_name}")
             if channel.title:
